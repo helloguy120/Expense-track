6f6106d59731f45197f6e181a7fb8b9c2cd61161
Confessions <assalamuhello123@gmail.com>
2026-02-04 21:09:19 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+import { getStorage } from
+"https://www.gstatic.com/firebasejs/12.8.0/firebase-storage.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyAB263ElAc0VNcMC5nB4w9FZW7qDHf76RY",
+authDomain: "expense-tracking-ea7de.firebaseapp.com",
+projectId: "expense-tracking-ea7de",
+storageBucket: "expense-tracking-ea7de.firebasestorage.app",
+messagingSenderId: "566671004347",
+appId: "1:566671004347:web:e9dd0ed4205a0ffadb0f9e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
